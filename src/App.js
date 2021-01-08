6197670753aa38908f56d289508645344236747f
aditya <adityafernanda2013@gmail.com>
2021-01-08 10:11:39 +0700

    react-router fixed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,28 @@ class App extends React.Component{
   render(){
     return(
       <div>
-        <Utama/>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a className="nav-link"><Link to="/">Beranda</Link></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" ><Link to="/tentangsaya">Tentang Saya</Link></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link"><Link to="/karya">Karya</Link></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link"><Link to="/kontak">kontak</Link></a>
+            </li>
+            {/* <p><Utama /></p> */}
+          </ul>
+        </div>
+      </div>
+      </nav>
+      <p><Utama /></p>
       </div>
     );
   }
--- src/components/Utama.js
+++ src/components/Utama.js
@@ -11,27 +11,10 @@ import kontak from './kontak';
 //mengatur beranda awal untuk switch
 const Utama = () => (
     <switch>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div class="container">
-              <a class="navbar-brand" href="./beranda.js">Beranda</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="./karya.js">Karya</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="./kontak.js">Kontak</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="./tentangsaya">Tentang Saya</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        <Route exact path="/" component={beranda} />
+        <Route path="/tentangsaya" component={tentangsaya} />
+        <Route path="/karya" component={karya} />
+        <Route path="/kontak" component={kontak} />
     </switch>
 )
 
